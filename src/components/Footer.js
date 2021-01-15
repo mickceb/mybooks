1d2da88a968d7e76a9e2ae8b65c065acc90f468a
mickceb <cebulski.mi@gmail.com>
2021-01-15 18:19:48 +0100

    Update: add class fixed-bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="footer text-white bg-primary mt-5">
+    <div className="footer text-white bg-primary mt-5 fixed-bottom">
       <div className="container p-3">
         <span>&copy; Mickceb</span>
       </div>
