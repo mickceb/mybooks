a16ce4737cad9fb4fd6344e1f6c82c0f2499c9c5
mickceb <cebulski.mi@gmail.com>
2021-01-15 15:24:55 +0100

    New component

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <header>
+      <div className="d-flex flex-column flex-md-row p-3 border-bottom bg-primary text-white">
+        <h4 className="mr-md-auto">
+          <a href="/" className="text-decoration-none text-white">
+            MyBooks
+          </a>
+        </h4>
+      </div>
+
+      {/* MENU */}
+    </header>
+  );
+};
+
+export default NavBar;
