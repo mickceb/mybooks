47ece10b8ff6afe4df99c5685994df26546b7140
mickceb <cebulski.mi@gmail.com>
2021-01-15 15:28:24 +0100

    New component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer text-white bg-primary mt-5">
+      <div className="container p-3">
+        <span>&opy; Mickceb</span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
