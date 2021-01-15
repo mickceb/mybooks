0935e3868eeda72e10268ccbad20e27f10c16d5c
mickceb <cebulski.mi@gmail.com>
2021-01-15 15:39:59 +0100

    Update: adding new component: addBooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
+import AddBooks from "./containers/AddBooks";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <AddBooks />
       <Footer />
     </div>
   );
