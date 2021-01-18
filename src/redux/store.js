b0629adc1fa50498b4d4d9e20d4b461c343cdd54
mickceb <cebulski.mi@gmail.com>
2021-01-18 13:16:33 +0100

    Update: add reducerFetchedBooks

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import reducerAddBooks from "./reducers/reducerAddBooks";
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
   library: reducerAddBooks,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
