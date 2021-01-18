894be53c62d023bd2e7ed419e9174a86a48962fb
mickceb <cebulski.mi@gmail.com>
2021-01-18 13:18:08 +0100

    Update: change name

---
+++ src/redux/reducers/reducerFetchBooks.js
@@ -0,0 +1,26 @@
+import {
+  FETCH_BOOKS_LOADING,
+  FETCH_BOOKS_SUCCESS,
+  FETCH_BOOKS_ERROR,
+} from "../constants";
+
+const initialState = {
+  isLoading: false,
+  fetchedBooks: [],
+  error: "",
+};
+
+const reducerFetchBooks = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_BOOKS_LOADING:
+      return { ...state, isLoading: true };
+    case FETCH_BOOKS_SUCCESS:
+      return { ...state, isLoading: true };
+    case FETCH_BOOKS_ERROR:
+      return { ...state };
+    default:
+      return state;
+  }
+};
+
+export default reducerFetchBooks;
