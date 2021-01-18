f5433649b8c9e9869bda8e690e1c4fd055b6e377
mickceb <cebulski.mi@gmail.com>
2021-01-18 13:19:10 +0100

    Update: add applyMiddleware & thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import reducerAddBooks from "./reducers/reducerAddBooks";
+import reducerFetchBooks from "./reducers/reducerFetchBooks";
 import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
   library: reducerAddBooks,
+  search: reducerFetchBooks,
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
